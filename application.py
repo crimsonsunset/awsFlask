daf742c362a99f43c0cb71454717cf70f6dd8998
Joe Sangiorgio <jsangio1@gmail.com>
2014-10-05 20:32:46 -0400

    adding endpoints

--- application.py
+++ application.py
@@ -1,1 +1,48 @@
-/Users/joe/Desktop/awsFlask/application.py
+import flask
+import socket
+from flask.ext.cors import CORS
+
+application = flask.Flask(__name__)
+cors = CORS(application)
+
+#Set application.debug=true to enable tracebacks on Beanstalk log output.
+#Make sure to remove this line before deploying to production.
+application.debug=True
+
+global token
+
+def netcat(hostname, port, content):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((hostname, port))
+    s.sendall(content)
+    s.shutdown(socket.SHUT_WR)
+    global token
+    while 1:
+        data = s.recv(1024)
+        if data == "":
+            break
+        token = data
+        print "Received:", data
+
+    # print(token)
+    print "Connection closed."
+    s.close()
+    return token
+
+
+@application.route('/')
+def return_token():
+    return netcat("challenge2.airtime.com",2323,"")
+
+@application.route('/token')
+def return_token():
+    return netcat("challenge2.airtime.com",2323,"")
+
+
+@application.route('/identify')
+def identify():
+    return netcat("challenge2.airtime.com",2323,"")
+
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0')
